9f0566cf45bb1fe0ec117a530008c52ed7311d9f
Marvin Juárez <25001064@falileo.edu>
2026-02-22 23:16:39 -0600

    Entrega Semana 1 - Backend Habitos

---
+++ app.js
@@ -0,0 +1,48 @@
+// Importar módulos
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var createError = require('http-errors');
+require('dotenv').config();
+require('./config/database'); // conexión a MongoDB
+
+// Crear app
+var app = express();
+
+// Middleware
+var cors = require('cors');
+app.use(cors());
+app.use(express.json()); // para recibir JSON
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View engine setup (opcional si usas plantillas)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Rutas
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const habitsRouter = require('./routes/habits');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/habits', habitsRouter);
+
+// Catch 404
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
